00b509742df4b1e686c10b19a505b34e943f229a
Luca <l.martini@nectelecomunicazioni.it>
2017-03-07 15:17:29 +0100

    added maven-antrun-plugin task and strategy

---
+++ src/main/java/it/clevercom/echo/rd/reveng/strategy/Custom_RMDW_Strategy.java
@@ -0,0 +1,32 @@
+package it.clevercom.echo.rd.reveng.strategy;
+
+import org.hibernate.cfg.reveng.DelegatingReverseEngineeringStrategy;
+import org.hibernate.cfg.reveng.ReverseEngineeringStrategy;
+import org.hibernate.cfg.reveng.TableIdentifier;
+
+public class Custom_RMDW_Strategy extends DelegatingReverseEngineeringStrategy {
+
+	public Custom_RMDW_Strategy(ReverseEngineeringStrategy delegate) {
+		super(delegate);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.cfg.reveng.DelegatingReverseEngineeringStrategy#columnToPropertyName(org.hibernate.cfg.reveng.TableIdentifier, java.lang.String)
+	 */
+	@Override
+	public String columnToPropertyName(TableIdentifier table, String column) {
+		// TODO Auto-generated method stub
+		return super.columnToPropertyName(table, column);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.cfg.reveng.DelegatingReverseEngineeringStrategy#tableToClassName(org.hibernate.cfg.reveng.TableIdentifier)
+	 */
+	@Override
+	public String tableToClassName(TableIdentifier tableIdentifier) {
+		String tableName = super.tableToClassName(tableIdentifier);
+		System.out.println(tableName.replace("Rd", ""));
+		return tableName.replace("Rd", "");
+	}
+}
